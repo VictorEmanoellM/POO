774b3001803e678a2218cef0c867afc0744a398d
Thayroni Lima <130916851+Thayroni-Lima@users.noreply.github.com>
2024-05-20 20:16:20 -0300

    Criação de Acervo e Livro

---
+++ Main/Thayroni/Livro.java
@@ -0,0 +1,19 @@
+public class Livro {
+    //Declaração dos atributos da classe
+    String titulo;
+    String autor;
+
+    //Métodos Getters e Setter dos atributos
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+}
