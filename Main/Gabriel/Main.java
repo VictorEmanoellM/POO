da4247938dbab0102872e05ec70dc4655c03aebd
bielalvz <132069822+bielalvz@users.noreply.github.com>
2024-05-21 23:59:30 -0300

    Update Main.java
    Adição dos atributos e das funcionalidades da classe User

--- Main/Gabriel/Main.java
+++ Main/Gabriel/Main.java
@@ -1,4 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
-    int id;
-    String nome;
+    public static void main(Sting[] args) {
+
+        User user1 = new User("Gabriel", "gabriel@exemple.com", "senha123");
+        User user2 = new User("Josinaldo", "josinaldo@exemple.com", "senha456");
+
+        Scaner scanner = new Scanner(System.in);
+        System.out.println("Bem vindo ao sistema bibilioteca!");
+
+        System.out.println("Digite seu email: ");
+        String email = scanner.nextLine();
+        System.out.println("Digite sua senha: ");
+        String senha = scanner.nextLine();
+
+        User currentUser = null;
+        if (User1.getEmail().equals(email) && user1.autenticar(senha)) {
+            currentUser = user1;
+        } else if (User2.getEmail().equals(email) && user1.autenticar(senha)) {
+            currentUser = user2;
+        }
+
+        if (currentUser == null) {
+            System.out.println("Email ou senha incorretos, Encerrando...");
+            return;
+        }
+
+    }
 }
