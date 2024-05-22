7c6ce7b15c206cbabf1f5d6558b406e3c3296f5a
bielalvz <132069822+bielalvz@users.noreply.github.com>
2024-05-22 00:05:04 -0300

    Create User.java
    Criação da classe User para atribuição na Main princiapal

---
+++ Main/Gabriel/User.java
@@ -0,0 +1,23 @@
+public class User {
+    private String nome;
+    private String email;
+    private String senha;
+
+    public class User (String nome, String email, String senha){
+        this.nome = nome;
+        this.email = email;
+        this. senha = senha;
+    }
+}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public boolean autenticar(String senha) {
+        return this.senha.equals(senha);
+}
